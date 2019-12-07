b4685334d62ae28607d809084c6eeb533f7c1efa
Jorge Ramirez <jorger99@umd.edu>
2019-12-07 15:23:28 -0500

    created initial code file

---
+++ PendulumPerturbation.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Pendulum_Perturbation.py is a script that does analysis on a double pendulum,
+specifically on the divergence in trajectories for double pendulums with similar
+but slightly varying initial conditions.
+
+Team Lead: Jorge Ramirez
+Engine code: Angel Torres
+Analysis: Maia Adams
+
+Supervised by Dan Lathrop as a part of the University of Maryland, College Park
+PHYS410: Classical Mechanics
+
+"""
+
+"""
+This code is split into three sections.
+
+    (0) simulation settings
+        a chunk of code dedicated to simulation settings that are global
+        throughout the entire script
+        
+    (1) numerical computation
+        a chunk of code dedicated to the numerical computation of 
+        double pendulum trajectories    
+    
+    (2) analytics
+        a chunk of code dedicated to running analytics on the computations,
+        e.g. lyapunov exponent, chaos measurements, etc
+        
+    (3) plotting algorithm
+        a chunk of code dedicated to creating visual representations of the
+        various double pendulum trajectories in an aesthetically-pleasing
+        realtime plot format.
+"""
+
+
+
+'''
+(0) simulation settings
+    Inputs -
+        user defined constants
+        universal constants
+    Outputs - 
+        global variables to be used in (1) (2) (3)
+        
+'''
+
+
+'''
+(1) numerical computation (Angel Torres)
+    Inputs - 
+        time series 
+'''
+
+
+'''
+(2) analytics (Maia Adams)
+'''
+
+
+'''
+(3) plotting algorithms (Jorge Ramirez)
+'''
