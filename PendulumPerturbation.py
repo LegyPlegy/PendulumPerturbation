bfab8fa1777169280838f7d544845c293fe13491
torang12 <angelctorres12@gmail.com>
2019-12-08 14:55:06 -0500

    Update PendulumPerturbation.py

--- PendulumPerturbation.py
+++ PendulumPerturbation.py
@@ -67,14 +67,18 @@ def double_pen(m1, m2, l1, l2, theta1, theta2, g):
     c2 = np.cos(2*theta1 - 2*theta2)
     S = np.sin(theta1-theta2)
     M = m1+m2
+#Simplifications for repetition between ODE for theta1doubledot and theta2doubledot   
     theta1d = T1
     theta2d = T2
     T1d = (-g*(2*m1+m2)*np.sin(theta1)-m2*g*S-2*S*m2*((T2**2)*l2+T1**2*l1*C))/(l1*(2*m1+m2-m2*c2))
     T2d = (2*S*((T1**2)*l1*M+g*M*np.cos(theta1)+(T2**2)*l2*m2*C))/(l2*(2*m1+m2-m2*c2))
-return theta1d, theta2d, T1d, T2d  
+#Four 1st Order Equations for the Coupled Second Order ODEs
+#T1 is the first derivative of theta1, T2 is the first for theta2
+#T1d is the second derivative of theta1, T2d is the second for theta 2
+    return theta1d, theta2d, T1d, T2d  
 
   
-    '''
+'''
     a method that simulates the trajectory of ONE pendulum for a given
     init cond and time series
         
@@ -82,9 +86,9 @@ return theta1d, theta2d, T1d, T2d
             time_series: 
                 [float array] a numpy array containing time steps
                 created using numpy.linspace
-        '''
-        np.linspace(0,5,501)
-        '''
+'''
+np.linspace(0,5,501)
+'''
             init_cond: 
                 [tuple] a tuple of the form (Є1, Є2) where 
                 Є1 is shift in θ1 and Є2 is shift in θ2
@@ -92,20 +96,20 @@ return theta1d, theta2d, T1d, T2d
             global_settings: 
                 [tuple] a tuple containing the simulation parameters
                         see section [0] for more info
-       '''
+'''
        
-       '''     
+'''     
         Outputs -
             sim_trajectory:
                 [2xn array] a 2D array where column 1 and 2 contain the final 
                 trajectory for θ1 and θ2, where n is the total steps
                 
-    '''
+'''
     
     
-    sim_trajectory = [0, 1, 2]  # contains computed values for f(θ1) and f(θ2) 
+sim_trajectory = [0, 1, 2]  # contains computed values for f(θ1) and f(θ2) 
     
-    return sim_trajectory
+return sim_trajectory
             
 '''
 ===============================================================================
